68a8f7de1fd273a7f013c79cef6a1e1b7fec4c44
ermz <eramirez4369@gmail.com>
2022-10-17 01:17:10 -0400

    Added basic Chainlink VRF implementation

--- contracts/Banca.sol
+++ contracts/Banca.sol
@@ -24,24 +24,48 @@ import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
 import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
 
 
-contract Banca is Ownable {
+contract Banca is VRFConsumerBaseV2, Ownable {
     enum Wagers { HIGH, LOW, ACES }
 
     address payable bancaOwner;
+    uint256 private s_lastRoll;
 
     // Chainlink variables
     uint64 s_subscriptionId;
     bytes32 s_keyHash;
     uint32 callbackGasLimit = 100000;
-    uint32 requestConfirmations = 3;
+    uint16 requestConfirmations = 3;
     uint32 constant RANDOM_VALUES = 3; // Three dice values
+
+    VRFCoordinatorV2Interface COORDINATOR;
 
     event GameResult(address player, Wagers wager, uint256 amountWon);
 
-    constructor(uint64 subscriptionId, bytes32 keyHash) {
+    constructor(uint64 subscriptionId, bytes32 keyHash) VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D) {
         s_subscriptionId = subscriptionId;
         s_keyHash = keyHash;
         bancaOwner = payable(msg.sender);
+
+        COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
+    }
+
+    function requestRandomRoll() internal onlyOwner returns (uint256 requestId){
+        requestId = COORDINATOR.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            RANDOM_VALUES
+        );
+    }
+
+    function fulfillRandomWords(uint256 /* _requestId */, uint256[] memory _randomWords) internal override {
+        // Number of sides on a dice 6
+        // RandomNum % 6 will equal a number between 0 and 5
+        // Add by 1 to get a number between 1 and 6
+        // Add three values to calculate result from roll
+        uint256 rollAmount = (_randomWords[0] % 6 + 1) + (_randomWords[1] % 6 + 1) + (_randomWords[2] % 6 + 1);
+        s_lastRoll = rollAmount;
     }
 
     function playFrancesa(Wagers _wager) payable public {
@@ -51,18 +75,24 @@ contract Banca is Ownable {
         uint256 sumResult;
 
         // Code will run while sumResult doesn't equal 3(Aces), 5-7(Low) or 14-16(High)
+        // added break temporarily, to not loop nonstop
         while (sumResult >= 3 || sumResult >= 5 && sumResult <= 7 || sumResult >= 14 && sumResult <= 16) {
+            requestRandomRoll();
+            sumResult = s_lastRoll;
             break;
         }
 
-        //
-
-        if (_wager == Wagers.HIGH || _wager == Wagers.LOW) {
+        if (sumResult >= 14 && sumResult <= 16 && uint256(_wager) >= 14 && uint256(_wager) <= 16) {
             // Pay one to one + some token
             // Percentage based on amount bet
             amountWon = msg.value * 2;
             payable(msg.sender).transfer(amountWon);
-        } else if (_wager == Wagers.ACES) {
+        } else if (sumResult >= 5 && sumResult <= 7 && uint256(_wager) >= 5 && uint256(_wager) <= 7) {
+            // Pay one to one + some token
+            // Percentage based on amount bet
+            amountWon = msg.value * 2;
+            payable(msg.sender).transfer(amountWon);
+        } else if (sumResult == 3 && uint256(_wager) == 3) {
             amountWon = msg.value * 61;
             payable(msg.sender).transfer(amountWon);
         }
